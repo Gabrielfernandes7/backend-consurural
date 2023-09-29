8d43a09f1e640c00570d0b9f546f28d0cd2d6ed1
Gabriel Fernandes <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2023-09-29 13:47:23 -0300

    base project

---
+++ consurural/HELP.md
@@ -0,0 +1,7 @@
+# Consurural - Backend
+
+## Crud com formulário (Usuários e consultorias)
+
+## Enviar mensagem personalizada para o WhatsApp
+
+## Testes 
---
+++ consurural/src/test/java/com/consurural/consurural/ConsururalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consurural.consurural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsururalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
