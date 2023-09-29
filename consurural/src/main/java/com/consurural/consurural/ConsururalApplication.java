a50627ef37e360ae0115905803a80474c3e490f9
Gabriel Fernandes <78227127+Gabrielfernandes7@users.noreply.github.com>
2023-09-29 14:03:33 -0300

    base project (#1)

---
+++ consurural/src/main/java/com/consurural/consurural/ConsururalApplication.java
@@ -0,0 +1,13 @@
+package com.consurural.consurural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsururalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsururalApplication.class, args);
+	}
+
+}
