413439a34f1d5426bdee25d6c66ddcec80afc76a
Ruan Ripardo <114603870+Ruanziinx@users.noreply.github.com>
2023-10-25 13:26:04 -0300

    refactor (#3)

---
+++ consurural/src/main/java/com/consurural/consurural/services/FormularioService.java
@@ -0,0 +1,20 @@
+package com.consurural.consurural.services;
+
+import com.consurural.consurural.models.Formulario;
+import com.consurural.consurural.repository.FormularioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FormularioService {
+
+    @Autowired
+    private FormularioRepository formularioRepository;
+
+    public Formulario salvarFormulario(Formulario formulario) {
+        return formularioRepository.save(formulario);
+    }
+
+}
